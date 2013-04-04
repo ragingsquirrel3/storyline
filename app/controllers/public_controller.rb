c8781cebdb4386cbeab06942aaa5c819d534bedd
Travis <travissheppard.1988@gmail.com>
2013-04-03 22:34:48 -0400

    le initial commit

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,8 @@
+class PublicController < ApplicationController
+  skip_before_filter :authenticate_user!
+  def index
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storyline::Application.config.secret_token = '1daff490d9c883b372d7f971653cd6422c4ec8fa77450ea1ea2188ed98481f8963c242e0beea28115bf0816300a93a0fb7421afe8c648fa8683fa90289c2e4e9'
